a6b491f27f50e26151bd1e7bb053e5ac0e7940fd
odd <793523185@qq.com>
2021-08-27 11:46:55 +0800

    feat: PageLoading 组件的开发 & 路由懒加载的配置

---
+++ src/components/PageLoading/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "@emotion/styled/macro";
+import { Player } from "@lottiefiles/react-lottie-player";
+
+import { CaptionText } from "../../styles/typography";
+
+import { GetComponentProps } from "../../utils";
+
+const lottiePlayProps: GetComponentProps<Player> = {
+  src: "https://assets3.lottiefiles.com/packages/lf20_dFnnfb.json",
+  autoplay: true,
+  loop: true,
+  speed: 0.5,
+  style: { width: 300, height: 300 },
+};
+
+const PageLoading: React.FC = () => {
+  return (
+    <Container>
+      <Player {...lottiePlayProps} />
+      <CaptionText>正在打开 fundsbox 中...</CaptionText>
+    </Container>
+  );
+};
+
+export default PageLoading;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { lazy } from "react";
 import { Route, Routes } from "react-router";
 import { HashRouter } from "react-router-dom";
 import Layout from "../layout";
-import HomePage from "../pages/HomePage";
-import NotFoundPage from "../pages/NotFoundPage";
+import PageLoading from "../components/PageLoading";
 
-// const HomePage = lazy(() => import("../pages/HomePage"));
-// const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
+const HomePage = lazy(() => import("../pages/HomePage"));
+const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
 
 const RootRouter: React.FC = () => {
   return (
-    <HashRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/*" element={<NotFoundPage />} />
-        </Routes>
-      </Layout>
-    </HashRouter>
+    <React.Suspense fallback={<PageLoading />}>
+      <HashRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/*" element={<NotFoundPage />} />
+          </Routes>
+        </Layout>
+      </HashRouter>
+    </React.Suspense>
   );
 };
 
